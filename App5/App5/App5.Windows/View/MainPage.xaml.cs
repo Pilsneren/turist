6ca931bb9078ecbf5b391cd703442a4f1d509cc1
Pilsneren <parbst2600@gmail.com>
2014-10-23 10:35:51 +0200

    Turistv3
    Vores turist app version 3 - take 1

---
+++ App5/App5/App5.Windows/View/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+using Windows.UI.Xaml.Controls;
+
+namespace App5.View
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
